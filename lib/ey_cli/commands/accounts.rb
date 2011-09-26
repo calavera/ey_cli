72c97a5d94ee203faf17d930d1e487d5af92568c
David Calavera <david.calavera@gmail.com>
2011-09-26 09:51:57 +0200

    add accounts command

---
+++ lib/ey_cli/commands/accounts.rb
@@ -0,0 +1,27 @@
+module EYCli
+  module Command
+    class Accounts < Base
+      def initialize
+        @accounts = EYCli::Controller::Accounts.new
+      end
+
+      def invoke
+        accounts = @accounts.fetch_accounts.map {|a| "  - #{a.name}"}.join("\n")
+        EYCli.term.say <<-EOF
+
+Available accounts:
+#{accounts}
+EOF
+
+      end
+
+      def help
+        <<-EOF
+
+Usage: ey_cli accounts
+Note: it shows the accounts that belong to you and also the ones that you are collaborating with.
+EOF
+      end
+    end
+  end
+end
